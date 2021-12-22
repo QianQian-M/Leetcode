84540c81086040d949db077f1d2cfbb12b82f303
Qian <mqmq225@gmail.com>
2021-12-21 21:12:33 -0500

    linked list

---
+++ 143_Reorder_List.py
@@ -0,0 +1,39 @@
+'''
+Combination of other three questions:
+
+1. middle of the linked list
+2. reverse linked list
+3.merge two sorted lists
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head):
+        #step 1: find middle
+        if not head: return []
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #step 2: reverse second half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt    
+        slow.next = None
+        
+        #step 3: merge lists
+        head1, head2 = head, prev
+        while head2:
+            nextt = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = nextt
+        
